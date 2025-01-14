3ea667d9e25745e692f4db653064b624a61eb840
epikijetesantakalu <157253342+epikijetesantakalu@users.noreply.github.com>
2025-01-14 15:22:23 +0900

    Update tokitosi.js

--- toki wawa/tokitosi.js
+++ toki wawa/tokitosi.js
@@ -67,6 +67,6 @@ window.onload = function() {
 };
 
 function A1RandomNoun() {
-  A1NounID = Math.Random() * A1Nouns.length - 1;
+  A1NounID = Math.random() * A1Nouns.length - 1;
   A1NounText.innerText = A1Nouns[A1NounID].name;
 };
