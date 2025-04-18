be71cbb62aac5e24a27f763a654443289a547187
epikijetesantakalu <157253342+epikijetesantakalu@users.noreply.github.com>
2025-04-18 13:52:18 +0900

    Update japanese.js

--- toki wawa/japanese.js
+++ toki wawa/japanese.js
@@ -1,14 +1,35 @@
 const Text = document.getElementById('Text');
 const Input = document.getElementById('Input');
+const Result = document.getElementById('Result');
+
+Input.addEventListener("keydown", test_event);
 
 const Words = [
-  {english: 'turtle', japanese: 'かめ'}
+  {english: 'turtle', japanese: 'かめ'},
+  {english: 'lover', japanese: 'あいじん'},
 ];
+
+let WordID;
 
 window.onload = function() {
   RandomWord();
 };
 
 function RandomWord() {
-  Text.innerText = Words[Math.floor(Math.random() * Words.length)].english;
+  WordID = Math.floor(Math.random() * Words.length);
+  Text.innerText = Words[WordID].english;
 };
+
+function test_event(e) {
+  if (e.key === "Enter") {
+    if (Input.value = Words[WordID].japanese) {
+      Result.value = Correct!
+      RandomWord();
+    }
+    else {
+      Result.value = Incorrect...
+      RandomWord();
+    }
+  }  
+  return false;
+}
