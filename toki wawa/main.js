58d225634af6be1663e23dfe1b34aba701ae926b
LilithAoto <LilithAoto@gmail.com>
2025-12-02 20:47:30 +0900

    copy button added!

---
+++ toki wawa/main.js
@@ -0,0 +1,4 @@
+function copyText() {
+  navigator.clipboard.writeText(`<a href="https://epiku.net/"><imgsrc="/sitelen/88x31.gif"height="31"alt="button to epiku.net"/></a>`);
+  alert("Copied to clipboard!");
+}
