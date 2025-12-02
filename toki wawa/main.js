6bfabcf70853476b0b0bd6be14b511497e7cef81
LilithAoto <LilithAoto@gmail.com>
2025-12-02 22:48:55 +0900

    kule ante!

--- toki wawa/main.js
+++ toki wawa/main.js
@@ -1,4 +1,16 @@
+const themes = ["pinkmode", "purplemode", "kaerumode"];
+let currentThemeIndex = 0;
+const body = document.body;
+
 function copyText() {
-  navigator.clipboard.writeText(`<a href="https://epiku.net/"><imgsrc="/sitelen/88x31.gif"height="31"alt="button to epiku.net"/></a>`);
-  alert("Copied to clipboard!");
+	navigator.clipboard.writeText(
+		`<a href="https://epiku.net/"><img src="https://epiku.net/sitelen/88x31.gif" height="31" alt="button to epiku.net"></a>`
+	);
+	alert("Copied to clipboard!");
+}
+
+function colours() {
+	body.classList.remove(themes[currentThemeIndex]);
+	currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+	body.classList.add(themes[currentThemeIndex]);
 }
